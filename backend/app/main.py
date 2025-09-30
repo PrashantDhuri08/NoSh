0e32db092215f836883b84c8cd0957ef8370f31f
Prashant Dhuri <148627279+PrashantDhuri08@users.noreply.github.com>
2025-09-30 22:49:55 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    # allow_origins=["*"], 
+    allow_origins = [
+    "http://localhost:3000",
+    "https://nosh-teal.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],  # Allow POST, GET, OPTIONS, etc.
     allow_headers=["*"],
